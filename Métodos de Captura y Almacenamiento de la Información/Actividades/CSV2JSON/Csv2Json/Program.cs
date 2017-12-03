97310bbf84321e84b183f68105d33ca97a860a3f
maximilianolovera@hotmail.com <maximilianolovera@hotmail.com>
2017-12-03 17:51:11 -0300

    Version 2, genera un diccionario a partir de las columnas de la primer fila.

--- Métodos de Captura y Almacenamiento de la Información/Actividades/CSV2JSON/Csv2Json/Program.cs
+++ Métodos de Captura y Almacenamiento de la Información/Actividades/CSV2JSON/Csv2Json/Program.cs
@@ -7,43 +7,73 @@ using Newtonsoft.Json;
 
 namespace Csv2Json
 {
-    class Dato
-    {
-        public string Longitud { get; set; }
-        public string Latitud { get; set; }
-        public string Enero { get; set; }
-        public string Febrero { get; set; }
-        public string Marzo { get; set; }
-        public string Abril { get; set; }
-        public string Mayo { get; set; }
-        public string Junio { get; set; }
-        public string Julio { get; set; }
-        public string Agosto { get; set; }
-        public string Septimbre { get; set; }
-        public string Octubre { get; set; }
-        public string Noviembre { get; set; }
-        public string Diciembre { get; set; }
-        public string Media { get; set; }
-        public string Desviacion { get; set; }
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
-            string pathFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "result.csv");
+            Console.WriteLine("Ingrese nombre de archivo");
+            string filename = Console.ReadLine();
+            string pathFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, filename);
 
             int counter = 0;
             string line;
-            List<Dato> data = new List<Dato>();
+            List<Dictionary<string, object>> data = new List<Dictionary<string, object>>();
+            string[] columns = new string[] { };
+            Console.WriteLine(DateTime.Now + " procesando archivo CSV");
 
             // Lectura del archivo linea por linea.  
             StreamReader file = new StreamReader(pathFile);
             while ((line = file.ReadLine()) != null)
             {
-                if (counter > 0 && counter < 6)
+                if (counter == 0)
                 {
-                    data.Add(ProcessData(line));
+                    columns = line.Split(',');
+                }
+                else
+                {
+                    var info = line.Split(',');
+                    Dictionary<string, object> rowData = new Dictionary<string, object>();
+                    for (int i = 0; i < columns.Length; i++)
+                    {
+                        if (info.Length == columns.Length)
+                        {
+                            switch (columns[i])
+                            {
+                                case "category_id":
+                                    rowData.Add(columns[i], int.Parse(info[i]));
+                                    break;
+                                case "publish_time":
+                                    rowData.Add(columns[i], DateTime.Parse(info[i]));
+                                    break;
+                                case "views":
+                                    rowData.Add(columns[i], int.Parse(info[i]));
+                                    break;
+                                case "likes":
+                                    rowData.Add(columns[i], int.Parse(info[i]));
+                                    break;
+                                case "dislikes":
+                                    rowData.Add(columns[i], int.Parse(info[i]));
+                                    break;
+                                case "comment_count":
+                                    rowData.Add(columns[i], int.Parse(info[i]));
+                                    break;
+                                case "comments_disabled":
+                                    rowData.Add(columns[i], bool.Parse(info[i]));
+                                    break;
+                                case "ratings_disabled":
+                                    rowData.Add(columns[i], bool.Parse(info[i]));
+                                    break;
+                                case "video_error_or_removed":
+                                    rowData.Add(columns[i], bool.Parse(info[i]));
+                                    break;                                         
+                                default:
+                                    rowData.Add(columns[i], info[i].Replace("\\",""));
+                                    break;
+                            }                            
+                        }
+                    }
+
+                    data.Add(rowData);
                 }
 
                 counter++;
@@ -51,36 +81,16 @@ namespace Csv2Json
 
             file.Close();
 
-            string destinationFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "converted.json");
+            string destinationFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, filename + "-converted.json");
 
+            Console.WriteLine(DateTime.Now + " serializando información");
             var csv = JsonConvert.SerializeObject(data);
 
+            Console.WriteLine(DateTime.Now + " guardando archivo en disco");
             File.WriteAllText(destinationFile, csv.ToString());
-        }
 
-        private static Dato ProcessData(string line)
-        {
-            string[] data = line.Split(',');
-
-            return new Dato()
-            {
-                Longitud = data[0].ToString(),
-                Latitud = data[1].ToString(),
-                Enero = data[2].ToString(),
-                Febrero = data[3].ToString(),
-                Marzo = data[4].ToString(),
-                Abril = data[5].ToString(),
-                Mayo = data[6].ToString(),
-                Junio = data[7].ToString(),
-                Julio = data[8].ToString(),
-                Agosto = data[9].ToString(),
-                Septimbre = data[10].ToString(),
-                Octubre = data[11].ToString(),
-                Noviembre = data[12].ToString(),
-                Diciembre = data[13].ToString(),
-                Media = data[14].ToString(),
-                Desviacion = data[15].ToString(),
-            };
+            Console.WriteLine(DateTime.Now + " Proceso finalizado. Archivo generado: "+ filename + "-converted.json");
+            Console.ReadLine();
         }
     }
 }
